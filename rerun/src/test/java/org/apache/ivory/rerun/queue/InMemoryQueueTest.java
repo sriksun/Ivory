742c0037b9621c5ad0e89c674b0ba360ce6e9f11
srikanth.sundarrajan <srikanth.sundarrajan@inmobi.com>
2013-04-12 12:42:25 +0530

    Fix issue with in-memory retry queue implementation

---
+++ rerun/src/test/java/org/apache/ivory/rerun/queue/InMemoryQueueTest.java
@@ -0,0 +1,7 @@
+package org.apache.ivory.rerun.queue;
+
+import org.apache.log4j.Logger;
+
+public class InMemoryQueueTest {
+    private static final Logger LOG = Logger.getLogger(InMemoryQueueTest.class);
+}
